467afbee5ca84aab4cdbda1a3d6a94fad56a63e5
paul practices programming <85654508+p-oca@users.noreply.github.com>
2023-06-05 01:12:57 +1000

    Added solution

---
+++ PalindromeSln.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if ((x < 0) || ((x != 0) && (x % 10 == 0)))
+            return false;
+        else {
+            vector<int> digits;
+            if (x == 0)
+                return true;
+            else {
+                while (x > 0) {
+                    digits.insert(digits.begin(), x % 10);
+                    x /= 10;
+                }
+                //reverse(digits.begin(), digits.end());
+                vector<int>::iterator j = digits.end() - 1;
+                for (vector<int>::iterator i = digits.begin(); i != digits.end(); i++) {
+                    if (*i != *j)
+                        return false;
+                    j--;
+                }
+                return true;
+            }
+        }
+    }
+};
