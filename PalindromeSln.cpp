e6d3686156ad7e2e9fa5a9f8034174f16d68dcee
paul practices programming <85654508+p-oca@users.noreply.github.com>
2023-06-05 02:34:29 +1000

    Update PalindromeSln.cpp
    Added comments

--- PalindromeSln.cpp
+++ PalindromeSln.cpp
@@ -3,24 +3,24 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if ((x < 0) || ((x != 0) && (x % 10 == 0)))
+        if ((x < 0) || ((x != 0) && (x % 10 == 0))) // check for negative or the number 10
             return false;
-        else {
+        else { // valid case
             vector<int> digits;
-            if (x == 0)
+            if (x == 0) // accept 0
                 return true;
-            else {
-                while (x > 0) {
+            else { // non-0 case
+                while (x > 0) { // begin loading x into a string digit-by-digit
                     digits.insert(digits.begin(), x % 10);
                     x /= 10;
                 }
-                vector<int>::iterator j = digits.end() - 1;
-                for (vector<int>::iterator i = digits.begin(); i != digits.end(); i++) {
-                    if (*i != *j)
+                vector<int>::iterator j = digits.end() - 1; // initialise j at last element in vector
+                for (vector<int>::iterator i = digits.begin(); i != digits.end(); i++) { // iterate through vector
+                    if (*i != *j) // check if value at element i is not equal to element j
                         return false;
-                    j--;
+                    j--; // reverse iterate
                 }
-                return true;
+                return true; // return true in the event no non-palindromic elements have been found
             }
         }
     }
